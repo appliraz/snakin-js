74d9120d0974a6a99face01aeddde0f8d3eea0a8
appliraz <114522959+appliraz@users.noreply.github.com>
2023-04-22 20:07:39 +0300

    Add files via upload

--- directions.js
+++ directions.js
@@ -1,20 +1,22 @@
+import { trackpad } from "./touch_module.js";
 export let snakedirection = 'STOP';
 export let prev_direction = '';
 export let new_direction = true;
 const direction = {37: "LEFT", 38: "UP", 39: "RIGHT", 40: "DOWN", 87: "UP", 65: "LEFT", 83:"DOWN", 68:"RIGHT"};
+const touch_directions = ["UP", "DOWN", "LEFT", "RIGHT"];
 let touchStartX, touchStartY, touchEndX, touchEndY;
 
 
 export function listenForDirection(){
   document.addEventListener('keydown', updateDirection);
-  document.addEventListener('touchstart', updateTouchStart, false);
-  document.addEventListener('touchend', handleTouchEnd, false);
+  trackpad.addEventListener('touchstart', updateTouchStart, false);
+  trackpad.addEventListener('touchend', handleTouchEnd, false);
 }
 
 export function unlistenForDirection(){
   document.removeEventListener('keydown', updateDirection);
-  document.removeEventListener('touchstart', updateTouchStart, false);
-  document.removeEventListener('touchend', handleTouchEnd, false);
+  trackpad.removeEventListener('touchstart', updateTouchStart, false);
+  trackpad.removeEventListener('touchend', handleTouchEnd, false);
 }
 
 /**
@@ -63,11 +65,13 @@ export function restartDirections(){
 
 
 function updateTouchStart(event){
-    touchStartX = event.touches[0].clientX;
-    touchStartY = event.touches[0].clientY;
+  event.preventDefault();
+  touchStartX = event.touches[0].clientX;
+  touchStartY = event.touches[0].clientY;
 }
 
 function updateTouchEnd(event){
+  event.preventDefault();
   touchEndX = event.changedTouches[0].clientX;
   touchEndY = event.changedTouches[0].clientY;
 }
@@ -75,19 +79,26 @@ function updateTouchEnd(event){
 function getTouchDirection(){
   let deltaX = touchEndX - touchStartX;
   let deltaY = touchEndY - touchStartY;
-  return Math.abs(deltaX) > Math.abs(deltaY) ? (delatX>0 ? "RIGHT" : "LEFT") : (deltaY > 0 ? "DOWN" : "UP");
+  return Math.abs(deltaX) > Math.abs(deltaY) ? (deltaX>0 ? "RIGHT" : "LEFT") : (deltaY > 0 ? "DOWN" : "UP");
 }
 
+/**
+ * 
+ * @param {event} event 
+ *
+ */
 function handleTouchEnd(event){
   if(snakedirection=="END"){
     return;
   }
   updateTouchEnd(event);
   let dir = getTouchDirection();
-  if(!isDirection(dir))
+  console.log(dir)
+  if(!touch_directions.includes(dir))
     return;
   prev_direction = snakedirection;
-  let new_dir = direction[dir];
+  let new_dir = dir;
   snakedirection = new_dir;
   new_direction = (prev_direction!=snakedirection);
+  console.log("reached");
 }
